40a5b89bc86bca6c8373094532b2cc32d4dfaa0e
Ruth Buenaflor <roofio@Ruths-iMac.local>
2017-08-23 10:40:28 -0400

    correction to variable in routing/apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 
 var friendData = require("../data/friends.js");
-// var userInput = require("../public/survey.html");
+
 
 module.exports = function(app) {
 
@@ -23,32 +23,14 @@ module.exports = function(app) {
       }
     }
 
-    // friendData.push(req.body);
 
-    // var diff = [];
-    //
-    // for(var x=0; x<req.body.scores.length; x++){
-    //   diff.push(parseInt(req.body.scores[x]));
-    // }
-    //
-    // var theOne = {
-    //   name: req.body.name,
-    //   image: req.body.photo,
-    //   scores: diff
-    // };
-    //
-    // var bff = [];
-
-
-  //
-  //   // Intial Loop for first person
 
     var diff = [];
 
     for (var i = 0; i<friendData.length; i++) {
       var goodFriend = friendData[i];
 
-      // friendData.forEach(function(user) {
+
         var totalDiff = 0;
 
         for (var j = 0; j < goodFriend.scores.length; j++) {
@@ -58,11 +40,7 @@ module.exports = function(app) {
         diff[i]=totalDiff;
       }
 
-        //   console.log(friendData[i].name);
-        //   diff = 0;
-        //
-        // // Compare total to other users
-        // for (var i = 0; i < friendData.length-1; i++) {
+
 
           var diffUser = diff[0];
           var bff = 0;
@@ -78,53 +56,6 @@ module.exports = function(app) {
 
           res.json(friendData[bff]);
 
-          //   var allLeft= Math.abs(friendData[i].scores[j]-theOne.scores[j]);
-          //   oneUser.push(allLeft);
-          //   diffUser+=allLeft;
-          // }
 
-          // bff.push(diffUser);
-          //
-          // Array.min=function(bff){
-          //   return Math.min.apply(Math, bff);
-          // };
-          //
-          // var scoreOne=Array.min(bff);
-          // var scoreOther=bff.indexOf(scoreOne);
-          // var funOne=friendData[scoreOther];
-          //
-          // res.json(funOne);
-          //
-          // diffUser=0;
       })
   }
-
-//           var scoreOne = user.answers[i];
-//           var scoreOther = scoreOther.answers[i];
-//           var difference = +scoreOne - +scoreOther;
-//           totalDiff += diff;
-//         }
-//         // var userScore = userInput.score;
-//
-//         diff.push(totalDiff);
-//       });
-//
-//       var closestBff = Math.min.apply(null, diff);
-//
-//       var bff = [];
-//
-//       // Second for loop for person to match against
-//       for (var i = 0; i < diff.length; i++) {
-//         if (diff[i] === closestBff) {
-//           bff.push(friendData[i]);
-//
-//       res.json(bff);
-//
-//     } else {
-//
-//       res.json(theOne);
-//     }
-//     friendData.push(userInput);
-//   };
-//
-// };
